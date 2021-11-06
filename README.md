8808ee25a0b42032331654b16995ab0f8d50426e
Tomek Karwowski <to.karwowski@gmail.com>
2021-11-06 17:30:36 +0100

    add parser and readme

---
+++ README.md
@@ -0,0 +1,32 @@
+Usage:
+```
+$ python3 graph-distance.py -h
+usage: graph-distance.py [-h] [--exact] [--approximate] [--file FILE] [--random SIZE]
+
+Calculate distance between two graphs.
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --exact        calculate exact distance (O(n!) time complexity)
+  --approximate  calculate approximate distance (O(n^2) time complexity)
+  --file FILE    read graphs from file (see examples/ for input format)
+  --random SIZE  generate random isomorphic graphs
+```
+
+Examples:
+```
+$ python3 graph-distance.py --random 5
+exact distance:  0
+approximate distance:  8
+
+$ python3 graph-distance.py --random 5 --exact
+exact distance:  0
+
+$ python3 graph-distance.py --random 7 --approximate
+exact distance:  0
+approximate distance:  8
+
+$ python3 graph-distance.py --file examples/graph0.txt
+exact distance:  0
+approximate distance:  6
+```
---
+++ graph-distance.py
@@ -0,0 +1,68 @@
+import argparse
+import sys
+import numpy as np
+from graph import Graph
+from distance import exact_distance, aprox_distance
+
+
+def read_graphs_from_file(filename: str) -> [Graph, Graph]:
+    with open(filename, "r") as file_handle:
+        lines = file_handle.readlines()
+        # read first graph
+        graph1 = Graph(size=int(lines[0]))
+        for i in range(graph1.size):
+            line = lines[i + 1].rstrip()
+            graph1.matrix[i] = list(map(int, list(line)))
+
+        # read second graph
+        graph2 = Graph(size=int(lines[1 + graph1.size]))
+        for i in range(graph2.size):
+            line = lines[i + 2 + graph1.size].rstrip()
+            graph2.matrix[i] = list(map(int, list(line)))
+
+    return [graph1, graph2]
+
+
+def run_from_file(filename: str):
+    [graph1, graph2] = read_graphs_from_file(filename)
+    print("exact distance: ", exact_distance(graph1, graph2))
+    print("aprox distance: ", aprox_distance(graph1, graph2))
+
+
+def run_random(n):
+    graph1 = Graph(size=n)
+    graph1.matrix = np.random.randint(2, size=(n, n))
+    graph2 = graph1.random_permutation()
+
+
+def run(arguments):
+    if file_name := arguments.file:
+        [graph1, graph2] = read_graphs_from_file(file_name)
+    elif n := arguments.random:
+        graph1 = Graph(size=n)
+        graph1.matrix = np.random.randint(2, size=(n, n))
+        graph2 = graph1.random_permutation()
+    else:
+        print("either --file or --random must be passed")
+        return
+
+    if not arguments.exact or arguments.approximate:
+        arguments.exact = True
+        arguments.approximate = True
+
+    if arguments.exact:
+        print("exact distance: ", exact_distance(graph1, graph2))
+    if arguments.approximate:
+        print("approximate distance: ", aprox_distance(graph1, graph2))
+
+
+parser = argparse.ArgumentParser(description="Calculate distance between two graphs.")
+parser.add_argument("--exact", action="store_true", help="calculate exact distance (O(n!) time complexity)")
+parser.add_argument("--approximate", action="store_true", help="calculate approximate distance (O(n^2) time complexity)")
+parser.add_argument("--file", type=str, help="read graphs from file")
+parser.add_argument("--random", type=int, metavar='SIZE', help="generate random isomorphic graphs")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    run(args)
