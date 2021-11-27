21d0f93937045b08b26e097c42147a79ccc127c2
arkadiusz-gorecki <arkadiusz.gorecki52@gmail.com>
2021-11-27 21:22:08 +0100

    Updated non-iso graphs

--- tests/iso/tests_iso.sh
+++ tests/iso/tests_iso.sh
@@ -1,84 +1,84 @@
 echo 2
-python3 ../../graph-distance.py -qar 2 -o test-iso-n2-1.txt
-python3 ../../graph-distance.py -qar 2 -o test-iso-n2-2.txt
-python3 ../../graph-distance.py -qar 2 -o test-iso-n2-3.txt
-python3 ../../graph-distance.py -qar 2 -o test-iso-n2-4.txt
-python3 ../../graph-distance.py -qar 2 -o test-iso-n2-5.txt
+python3 ../../graph-distance.py -qa test-iso-n2-1.txt
+python3 ../../graph-distance.py -qa test-iso-n2-2.txt
+python3 ../../graph-distance.py -qa test-iso-n2-3.txt
+python3 ../../graph-distance.py -qa test-iso-n2-4.txt
+python3 ../../graph-distance.py -qa test-iso-n2-5.txt
 echo 3
-python3 ../../graph-distance.py -qar 3 -o test-iso-n3-1.txt
-python3 ../../graph-distance.py -qar 3 -o test-iso-n3-2.txt
-python3 ../../graph-distance.py -qar 3 -o test-iso-n3-3.txt
-python3 ../../graph-distance.py -qar 3 -o test-iso-n3-4.txt
-python3 ../../graph-distance.py -qar 3 -o test-iso-n3-5.txt
+python3 ../../graph-distance.py -qa test-iso-n3-1.txt
+python3 ../../graph-distance.py -qa test-iso-n3-2.txt
+python3 ../../graph-distance.py -qa test-iso-n3-3.txt
+python3 ../../graph-distance.py -qa test-iso-n3-4.txt
+python3 ../../graph-distance.py -qa test-iso-n3-5.txt
 echo 4
-python3 ../../graph-distance.py -qar 4 -o test-iso-n4-1.txt
-python3 ../../graph-distance.py -qar 4 -o test-iso-n4-2.txt
-python3 ../../graph-distance.py -qar 4 -o test-iso-n4-3.txt
-python3 ../../graph-distance.py -qar 4 -o test-iso-n4-4.txt
-python3 ../../graph-distance.py -qar 4 -o test-iso-n4-5.txt
+python3 ../../graph-distance.py -qa test-iso-n4-1.txt
+python3 ../../graph-distance.py -qa test-iso-n4-2.txt
+python3 ../../graph-distance.py -qa test-iso-n4-3.txt
+python3 ../../graph-distance.py -qa test-iso-n4-4.txt
+python3 ../../graph-distance.py -qa test-iso-n4-5.txt
 echo 5
-python3 ../../graph-distance.py -qar 5 -o test-iso-n5-1.txt
-python3 ../../graph-distance.py -qar 5 -o test-iso-n5-2.txt
-python3 ../../graph-distance.py -qar 5 -o test-iso-n5-3.txt
-python3 ../../graph-distance.py -qar 5 -o test-iso-n5-4.txt
-python3 ../../graph-distance.py -qar 5 -o test-iso-n5-5.txt
+python3 ../../graph-distance.py -qa test-iso-n5-1.txt
+python3 ../../graph-distance.py -qa test-iso-n5-2.txt
+python3 ../../graph-distance.py -qa test-iso-n5-3.txt
+python3 ../../graph-distance.py -qa test-iso-n5-4.txt
+python3 ../../graph-distance.py -qa test-iso-n5-5.txt
 echo 6
-python3 ../../graph-distance.py -qar 6 -o test-iso-n6-1.txt
-python3 ../../graph-distance.py -qar 6 -o test-iso-n6-2.txt
-python3 ../../graph-distance.py -qar 6 -o test-iso-n6-3.txt
-python3 ../../graph-distance.py -qar 6 -o test-iso-n6-4.txt
-python3 ../../graph-distance.py -qar 6 -o test-iso-n6-5.txt
+python3 ../../graph-distance.py -qa test-iso-n6-1.txt
+python3 ../../graph-distance.py -qa test-iso-n6-2.txt
+python3 ../../graph-distance.py -qa test-iso-n6-3.txt
+python3 ../../graph-distance.py -qa test-iso-n6-4.txt
+python3 ../../graph-distance.py -qa test-iso-n6-5.txt
 echo 7
-python3 ../../graph-distance.py -qar 7 -o test-iso-n7-1.txt
-python3 ../../graph-distance.py -qar 7 -o test-iso-n7-2.txt
-python3 ../../graph-distance.py -qar 7 -o test-iso-n7-3.txt
-python3 ../../graph-distance.py -qar 7 -o test-iso-n7-4.txt
-python3 ../../graph-distance.py -qar 7 -o test-iso-n7-5.txt
+python3 ../../graph-distance.py -qa test-iso-n7-1.txt
+python3 ../../graph-distance.py -qa test-iso-n7-2.txt
+python3 ../../graph-distance.py -qa test-iso-n7-3.txt
+python3 ../../graph-distance.py -qa test-iso-n7-4.txt
+python3 ../../graph-distance.py -qa test-iso-n7-5.txt
 echo 8
-python3 ../../graph-distance.py -qar 8 -o test-iso-n8-1.txt
-python3 ../../graph-distance.py -qar 8 -o test-iso-n8-2.txt
-python3 ../../graph-distance.py -qar 8 -o test-iso-n8-3.txt
-python3 ../../graph-distance.py -qar 8 -o test-iso-n8-4.txt
-python3 ../../graph-distance.py -qar 8 -o test-iso-n8-5.txt
+python3 ../../graph-distance.py -qa test-iso-n8-1.txt
+python3 ../../graph-distance.py -qa test-iso-n8-2.txt
+python3 ../../graph-distance.py -qa test-iso-n8-3.txt
+python3 ../../graph-distance.py -qa test-iso-n8-4.txt
+python3 ../../graph-distance.py -qa test-iso-n8-5.txt
 echo 9
-python3 ../../graph-distance.py -qar 9 -o test-iso-n9-1.txt
-python3 ../../graph-distance.py -qar 9 -o test-iso-n9-2.txt
-python3 ../../graph-distance.py -qar 9 -o test-iso-n9-3.txt
-python3 ../../graph-distance.py -qar 9 -o test-iso-n9-4.txt
-python3 ../../graph-distance.py -qar 9 -o test-iso-n9-5.txt
+python3 ../../graph-distance.py -qa test-iso-n9-1.txt
+python3 ../../graph-distance.py -qa test-iso-n9-2.txt
+python3 ../../graph-distance.py -qa test-iso-n9-3.txt
+python3 ../../graph-distance.py -qa test-iso-n9-4.txt
+python3 ../../graph-distance.py -qa test-iso-n9-5.txt
 echo 10
-python3 ../../graph-distance.py -qar 10 -o test-iso-n10-1.txt
-python3 ../../graph-distance.py -qar 10 -o test-iso-n10-2.txt
-python3 ../../graph-distance.py -qar 10 -o test-iso-n10-3.txt
-python3 ../../graph-distance.py -qar 10 -o test-iso-n10-4.txt
-python3 ../../graph-distance.py -qar 10 -o test-iso-n10-5.txt
+python3 ../../graph-distance.py -qa test-iso-n10-1.txt
+python3 ../../graph-distance.py -qa test-iso-n10-2.txt
+python3 ../../graph-distance.py -qa test-iso-n10-3.txt
+python3 ../../graph-distance.py -qa test-iso-n10-4.txt
+python3 ../../graph-distance.py -qa test-iso-n10-5.txt
 echo 11
-python3 ../../graph-distance.py -qar 11 -o test-iso-n11-1.txt
-python3 ../../graph-distance.py -qar 11 -o test-iso-n11-2.txt
-python3 ../../graph-distance.py -qar 11 -o test-iso-n11-3.txt
-python3 ../../graph-distance.py -qar 11 -o test-iso-n11-4.txt
-python3 ../../graph-distance.py -qar 11 -o test-iso-n11-5.txt
+python3 ../../graph-distance.py -qa test-iso-n11-1.txt
+python3 ../../graph-distance.py -qa test-iso-n11-2.txt
+python3 ../../graph-distance.py -qa test-iso-n11-3.txt
+python3 ../../graph-distance.py -qa test-iso-n11-4.txt
+python3 ../../graph-distance.py -qa test-iso-n11-5.txt
 echo 12
-python3 ../../graph-distance.py -qar 12 -o test-iso-n12-1.txt
-python3 ../../graph-distance.py -qar 12 -o test-iso-n12-2.txt
-python3 ../../graph-distance.py -qar 12 -o test-iso-n12-3.txt
-python3 ../../graph-distance.py -qar 12 -o test-iso-n12-4.txt
-python3 ../../graph-distance.py -qar 12 -o test-iso-n12-5.txt
+python3 ../../graph-distance.py -qa test-iso-n12-1.txt
+python3 ../../graph-distance.py -qa test-iso-n12-2.txt
+python3 ../../graph-distance.py -qa test-iso-n12-3.txt
+python3 ../../graph-distance.py -qa test-iso-n12-4.txt
+python3 ../../graph-distance.py -qa test-iso-n12-5.txt
 echo 13
-python3 ../../graph-distance.py -qar 13 -o test-iso-n13-1.txt
-python3 ../../graph-distance.py -qar 13 -o test-iso-n13-2.txt
-python3 ../../graph-distance.py -qar 13 -o test-iso-n13-3.txt
-python3 ../../graph-distance.py -qar 13 -o test-iso-n13-4.txt
-python3 ../../graph-distance.py -qar 13 -o test-iso-n13-5.txt
+python3 ../../graph-distance.py -qa test-iso-n13-1.txt
+python3 ../../graph-distance.py -qa test-iso-n13-2.txt
+python3 ../../graph-distance.py -qa test-iso-n13-3.txt
+python3 ../../graph-distance.py -qa test-iso-n13-4.txt
+python3 ../../graph-distance.py -qa test-iso-n13-5.txt
 echo 14
-python3 ../../graph-distance.py -qar 14 -o test-iso-n14-1.txt
-python3 ../../graph-distance.py -qar 14 -o test-iso-n14-2.txt
-python3 ../../graph-distance.py -qar 14 -o test-iso-n14-3.txt
-python3 ../../graph-distance.py -qar 14 -o test-iso-n14-4.txt
-python3 ../../graph-distance.py -qar 14 -o test-iso-n14-5.txt
+python3 ../../graph-distance.py -qa test-iso-n14-1.txt
+python3 ../../graph-distance.py -qa test-iso-n14-2.txt
+python3 ../../graph-distance.py -qa test-iso-n14-3.txt
+python3 ../../graph-distance.py -qa test-iso-n14-4.txt
+python3 ../../graph-distance.py -qa test-iso-n14-5.txt
 echo 15
-python3 ../../graph-distance.py -qar 15 -o test-iso-n15-1.txt
-python3 ../../graph-distance.py -qar 15 -o test-iso-n15-2.txt
-python3 ../../graph-distance.py -qar 15 -o test-iso-n15-3.txt
-python3 ../../graph-distance.py -qar 15 -o test-iso-n15-4.txt
-python3 ../../graph-distance.py -qar 15 -o test-iso-n15-5.txt
+python3 ../../graph-distance.py -qa test-iso-n15-1.txt
+python3 ../../graph-distance.py -qa test-iso-n15-2.txt
+python3 ../../graph-distance.py -qa test-iso-n15-3.txt
+python3 ../../graph-distance.py -qa test-iso-n15-4.txt
+python3 ../../graph-distance.py -qa test-iso-n15-5.txt
--- tests/non-iso/tests_non_iso.sh
+++ tests/non-iso/tests_non_iso.sh
@@ -1,48 +1,48 @@
-echo 2 exact
-python3 ../../graph-distance.py -qar 2 -o test-non-iso-n2-1.txt
-python3 ../../graph-distance.py -qar 2 -o test-non-iso-n2-2.txt
-python3 ../../graph-distance.py -qar 2 -o test-non-iso-n2-3.txt
-python3 ../../graph-distance.py -qar 2 -o test-non-iso-n2-4.txt
-python3 ../../graph-distance.py -qar 2 -o test-non-iso-n2-5.txt
-echo 3 exact
-python3 ../../graph-distance.py -qar 3 -o test-non-iso-n3-1.txt
-python3 ../../graph-distance.py -qar 3 -o test-non-iso-n3-2.txt
-python3 ../../graph-distance.py -qar 3 -o test-non-iso-n3-3.txt
-python3 ../../graph-distance.py -qar 3 -o test-non-iso-n3-4.txt
-python3 ../../graph-distance.py -qar 3 -o test-non-iso-n3-5.txt
-echo 4 exact
-python3 ../../graph-distance.py -qar 4 -o test-non-iso-n4-1.txt
-python3 ../../graph-distance.py -qar 4 -o test-non-iso-n4-2.txt
-python3 ../../graph-distance.py -qar 4 -o test-non-iso-n4-3.txt
-python3 ../../graph-distance.py -qar 4 -o test-non-iso-n4-4.txt
-python3 ../../graph-distance.py -qar 4 -o test-non-iso-n4-5.txt
-echo 5 exact
-python3 ../../graph-distance.py -qar 5 -o test-non-iso-n5-1.txt
-python3 ../../graph-distance.py -qar 5 -o test-non-iso-n5-2.txt
-python3 ../../graph-distance.py -qar 5 -o test-non-iso-n5-3.txt
-python3 ../../graph-distance.py -qar 5 -o test-non-iso-n5-4.txt
-python3 ../../graph-distance.py -qar 5 -o test-non-iso-n5-5.txt
-echo 6 exact
-python3 ../../graph-distance.py -qar 6 -o test-non-iso-n6-1.txt
-python3 ../../graph-distance.py -qar 6 -o test-non-iso-n6-2.txt
-python3 ../../graph-distance.py -qar 6 -o test-non-iso-n6-3.txt
-python3 ../../graph-distance.py -qar 6 -o test-non-iso-n6-4.txt
-python3 ../../graph-distance.py -qar 6 -o test-non-iso-n6-5.txt
-echo 7 exact
-python3 ../../graph-distance.py -qar 7 -o test-non-iso-n7-1.txt
-python3 ../../graph-distance.py -qar 7 -o test-non-iso-n7-2.txt
-python3 ../../graph-distance.py -qar 7 -o test-non-iso-n7-3.txt
-python3 ../../graph-distance.py -qar 7 -o test-non-iso-n7-4.txt
-python3 ../../graph-distance.py -qar 7 -o test-non-iso-n7-5.txt
-echo 8 exact
-python3 ../../graph-distance.py -qar 8 -o test-non-iso-n8-1.txt
-python3 ../../graph-distance.py -qar 8 -o test-non-iso-n8-2.txt
-python3 ../../graph-distance.py -qar 8 -o test-non-iso-n8-3.txt
-python3 ../../graph-distance.py -qar 8 -o test-non-iso-n8-4.txt
-python3 ../../graph-distance.py -qar 8 -o test-non-iso-n8-5.txt
-echo 9 exact
-python3 ../../graph-distance.py -qar 9 -o test-non-iso-n9-1.txt
-python3 ../../graph-distance.py -qar 9 -o test-non-iso-n9-2.txt
-python3 ../../graph-distance.py -qar 9 -o test-non-iso-n9-3.txt
-python3 ../../graph-distance.py -qar 9 -o test-non-iso-n9-4.txt
-python3 ../../graph-distance.py -qar 9 -o test-non-iso-n9-5.txt
+echo 2
+python3 ../../graph-distance.py -qaey test-non-iso-n2-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n2-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n2-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n2-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n2-5.txt
+echo 3
+python3 ../../graph-distance.py -qaey test-non-iso-n3-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n3-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n3-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n3-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n3-5.txt
+echo 4
+python3 ../../graph-distance.py -qaey test-non-iso-n4-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n4-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n4-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n4-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n4-5.txt
+echo 5
+python3 ../../graph-distance.py -qaey test-non-iso-n5-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n5-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n5-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n5-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n5-5.txt
+echo 6
+python3 ../../graph-distance.py -qaey test-non-iso-n6-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n6-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n6-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n6-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n6-5.txt
+echo 7
+python3 ../../graph-distance.py -qaey test-non-iso-n7-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n7-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n7-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n7-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n7-5.txt
+echo 8
+python3 ../../graph-distance.py -qaey test-non-iso-n8-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n8-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n8-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n8-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n8-5.txt
+echo 9
+python3 ../../graph-distance.py -qaey test-non-iso-n9-1.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n9-2.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n9-3.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n9-4.txt
+python3 ../../graph-distance.py -qaey test-non-iso-n9-5.txt
