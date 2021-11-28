0349b84e96093e418fe8db545e509b879d5a6199
arkadiusz-gorecki <arkadiusz.gorecki52@gmail.com>
2021-11-28 16:42:48 +0100

    Added diff size iso tests

---
+++ tests/diff-size/tests_diff_size.sh
@@ -0,0 +1,54 @@
+echo 6-5
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m5-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m5-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m5-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m5-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m5-5.txt
+echo 6-4
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m4-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m4-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m4-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m4-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m4-5.txt
+echo 6-3
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m3-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m3-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m3-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m3-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n6-m3-5.txt
+echo 7-6
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m6-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m6-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m6-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m6-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m6-5.txt
+echo 7-5
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m5-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m5-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m5-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m5-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m5-5.txt
+echo 7-4
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m4-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m4-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m4-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m4-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n7-m4-5.txt
+echo 8-7
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m7-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m7-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m7-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m7-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m7-5.txt
+echo 8-6
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m6-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m6-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m6-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m6-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m6-5.txt
+echo 8-5
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m5-1.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m5-2.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m5-3.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m5-4.txt
+python3 ../../graph-distance.py -qaey test-diff-size-n8-m5-5.txt
